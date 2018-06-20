f6918d027f884289e02ae4fde3088e5a7b209f59
William Dupré <william.dupre_ext@michelin.com>
2018-06-20 13:05:50 +0200

    some python exercices from checkio

---
+++ python/brackets.py
@@ -0,0 +1,16 @@
+def checkio(expression):
+    #Checking like I would do it opening brackets add 1 to the correct var 
+    #no closind allowed while 
+
+    for index in range(len(expression)):
+        print(index)
+    return True or False
+
+#These "asserts" using only for self-checking and not necessary for auto-testing
+if __name__ == '__main__':
+    assert checkio("((5+3)*2+1)") == True, "Simple"
+    assert checkio("{[(3+1)+2]+}") == True, "Different types"
+    assert checkio("(3+{1-1)}") == False, ") is alone inside {}"
+    assert checkio("[1+1]+(2*2)-{3/3}") == True, "Different operators"
+    assert checkio("(({[(((1)-2)+3)-3]/3}-3)") == False, "One is redundant"
+    assert checkio("2+3") == True, "No brackets, no problem"
