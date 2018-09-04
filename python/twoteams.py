139cc0ea1f15034059a4056bc22efa038f8780ee
William Dupré <william.dupre_ext@michelin.com>
2018-09-04 09:40:28 +0200

    Adding first Escher Solution

---
+++ python/twoteams.py
@@ -0,0 +1,36 @@
+def two_teams(sailors):
+    #replace this for solution
+    team1 = []
+    team2 = []
+    
+    for member in sailors:
+        if sailors[member] < 20 or sailors[member] > 40:
+            team1.append(member)
+        else:
+            team2.append(member)
+
+    return [sorted(team1),sorted(team2)]
+
+if __name__ == '__main__':
+    print("Example:")
+    print(two_teams({'Smith': 34, 'Wesson': 22, 'Coleman': 45, 'Abrahams': 19}))
+
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert two_teams({
+        'Smith': 34, 
+        'Wesson': 22, 
+        'Coleman': 45, 
+        'Abrahams': 19}) == [
+            ['Abrahams', 'Coleman'], 
+            ['Smith', 'Wesson']
+            ]
+
+    assert two_teams({
+        'Fernandes': 18,
+        'Johnson': 22,
+        'Kale': 41,
+        'McCortney': 54}) == [
+            ['Fernandes', 'Kale', 'McCortney'], 
+            ['Johnson']
+            ]
+    print("Coding complete? Click 'Check' to earn cool rewards!")
