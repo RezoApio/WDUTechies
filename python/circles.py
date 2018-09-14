f45427d5fe1fb7f6418ac5679cda04c08ea22432
William Dupré <william.dupre_ext@michelin.com>
2018-09-14 17:16:42 +0200

    Adding solution w/o handling // points

--- python/circles.py
+++ python/circles.py
@@ -1,3 +1,21 @@
+import math
+
+__DEBUG__ = True
+
+def log(text: str):
+    if __DEBUG__:
+        print(text)
+
+def mystr(num: float, var: str):
+    if num < 0:
+        ret=var+"+"
+    else:
+        ret=var+"-"
+    if num - math.trunc(num) < 0.01:
+        num=math.trunc(num)
+    ret=ret+str(num)
+    return ret
+
 def checkio(data):
    
     #replace this for solution
@@ -11,20 +29,28 @@ def checkio(data):
     x3=int(data[13])
     y3=int(data[15])
 
-    print(str(x1)+";"+str(y1)+";"+str(x2)+":"+str(y2)+";"+str(x3)+";"+str(y3))
+    log(str(x1)+":"+str(y1)+";"+str(x2)+":"+str(y2)+";"+str(x3)+":"+str(y3))
 
 
     #Calculus extracted from https://cral.univ-lyon1.fr/labo/fc/Ateliers_archives/ateliers_2005-06/cercle_3pts.pdf
 
-    xc=((x3**2-x2**2+y3**2-y2**2)/(2*(y3-y2))-(x2**2-x1**2+y2**2-y1**2)/(2*(y2-y1)))/((x2-x1)/(y2-y1)-(x3-x2)/(y3-y2))
-    print((x3**2-x2**2+y3**2-y2**2)/(2*(y3-y2)))
-    print((x2**2-x1**2+y2**2-y1**2)/(2*(y2-y1)))
-    print(xc)
+    num1=x3**2-x2**2+y3**2-y2**2; log(num1)
+    den1=2*(y3-y2); log(den1)
+    num2=x2**2-x1**2+y2**2-y1**2; log(num2)
+    den2=2*(y2-y1); log(den2)
+    delta1=(x2-x1)/(y2-y1); log(delta1)
+    delta2=(x3-x2)/(y3-y2); log(delta2)
+    xc=((num1/den1) - (num2/den2))/(delta1-delta2)
+    xc=-xc
+    log(xc)
     
-    ##-((x2**2-x1**2+y2**2-y1**2)/2*(y2-y1)))/(((x2-x1)/(y2-y1))-((x3-x2)/(y3-y2)))
-    yc=((x2**2-x1**2+y2**2-y1**2)/2*(y2-y1))-((x2-x1)/(y2-y1))*xc
-    print(yc)
+    delta4=(x2**2-x1**2+y2**2-y1**2)/(2*(y2-y1)); log(delta4)
+    delta3=(x2-x1)/(y2-y1); log(delta3)
+    yc=delta4-xc*delta3
+    log(yc)
+
     Rc=((x1-xc)**2+(y1-yc)**2)**(1/2)
+    log(Rc)
 
     #Rounding results
 
@@ -32,8 +58,9 @@ def checkio(data):
     yc=round(yc,2)
     Rc=round(Rc,2)
     
-    res="(x-"+str(xc)+")^2+(y-"+str(yc)+")^2="+str(Rc)+"^2"
-    print(res)
+
+    res="("+mystr(xc,"x")+")^2+("+mystr(yc,"y")+")^2="+str(Rc)+"^2"
+    log(res)
     return res
 
 #These "asserts" using only for self-checking and not necessary for auto-testing
